3197e312d31a8b5dced44385940ae4b90a32629d
Sajishrestha <Shresthasajish@users.noreply.github.com>
2022-06-13 10:32:39 +0545

    completed

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,24 +1,47 @@
 import React, {useState} from 'react'
-import { data } from '../data/carousel'
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 const Carousel = () => {
-  const [current, setCurrent] = useState(0)
-  const length = data.length
-  
-  return (
-    <div className='flex px-14 items-center justify-center'>
-      
-      {data?.map((item,index)=>(
-        <div className='flex flex-col justify-center items-center'>
-        <img src={item.image1} alt="" width="60%" className='mx-5'/>
-        <p>{item.heading1}</p>
-        <p>{item.heading2}</p>
-        <p>{item.heading3}</p>
-        </div>
-      ))} 
+
+  const handleDragStart = (e) => e.preventDefault();
+  const items = [
+    <div className='flex gap-5 items-center justify-center'>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
+    <img src="electrician2.jpg" onDragStart={handleDragStart} role="presentation" width='350px'/>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
+    </div>,
+    <div className='flex gap-5 items-center justify-center'>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
+    <img src="electrician2.jpg" onDragStart={handleDragStart} role="presentation" width='350px'/>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
+    </div>,
+    <div className='flex gap-5 items-center justify-center'>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
+    <img src="electrician2.jpg" onDragStart={handleDragStart} role="presentation" width='350px'/>
+    <img src="electrician.jpg" onDragStart={handleDragStart} role="presentation"  width="350px"/>
     </div>
-  )
+    
+  ];
+  
+  // return (
+    return (
+      <AliceCarousel mouseTracking items={items} disableButtonsControls autoPlay="true" autoPlayInterval="900"/>
+    );
+    // <div className='flex px-14 items-center justify-center'>
+      
+    //   {data?.map((item,index)=>(
+    //     <div className='flex flex-col justify-center items-center'>
+    //     <img src={item.image1} alt="" width="60%" className='mx-5'/>
+    //     <p>{item.heading1}</p>
+    //     <p>{item.heading2}</p>
+    //     <p>{item.heading3}</p>
+    //     </div>
+    //   ))} 
+    // </div>
+  // )
 }
 
 export default Carousel
--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import data from '../data/carousel'
 
 const Gallery = () => {
+
+  const initialState = {
+    All: false,
+    Residences: false,
+    Industrial: false,
+    Offices: false,
+    Retail:false,
+  };
+
+  const [isClicked, setIsClicked] = useState(initialState)
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
   return (
     <div className='mt-14'>
         <div className='flex flex-col items-center justify-center'>
             <p className='text-orange-500 font-bold'>@electricians</p>
             <p className='text-3xl font-semibold'>Our Projects</p>
             <div className='flex justify-around gap-5'>
-                <div className='font-semibold cursor-pointer'>All</div>
-                <div className='font-semibold cursor-pointer'>Residences</div>
-                <div className='font-semibold cursor-pointer'>Industrial Objects</div>
-                <div className='font-semibold cursor-pointer'>Offices</div>
-                <div className='font-semibold cursor-pointer'>Retail_objects</div>
+                <button className='font-semibold cursor-pointer' onClick={()=>handleClick('All')}>All</button>
+                <button className='font-semibold cursor-pointer' onClick={()=>handleClick('Residences')}>Residences</button>
+                <button className='font-semibold cursor-pointer' onClick={()=>handleClick('Industrial')}>Industrial Objects</button>
+                <button className='font-semibold cursor-pointer' onClick={()=>handleClick('Offices')}>Offices</button>
+                <button className='font-semibold cursor-pointer' onClick={()=>handleClick('Retail')}>Retail_objects</button>
             </div>
+            {isClicked.All &&
+             (
+             <div className='flex flex-col flex-wrap items-start'>
             <div className='flex gap-8 px-14 items-center justify-center mt-5'>
             <img src="electrician2.jpg" alt="" width="250px"/> 
             <img src="electrician2.jpg" alt="" width="250px"/> 
@@ -25,7 +39,76 @@ const Gallery = () => {
             <img src="electrician2.jpg" alt="" width="250px"/> 
             <img src="electrician2.jpg" alt="" width="250px"/> 
             <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div> 
+            </div>) }
+            {isClicked.Residences &&
+             (
+             <div className='flex flex-col flex-wrap'>
+            <div className='flex gap-8 px-14 items-center justify-center mt-5'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
             </div>
+            <div className='flex gap-8 px-14 items-center justify-center mt-10'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            
+            </div> 
+            </div>) }
+
+            {isClicked.Industrial &&
+             (
+             <div className='flex flex-col flex-wrap'>
+            <div className='flex gap-8 px-14 items-center justify-center mt-5'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div>
+            <div className='flex gap-8 px-14 items-center justify-center mt-10'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div> 
+            </div>) }
+
+            {isClicked.Offices &&
+             (
+             <div className='flex flex-col flex-wrap'>
+            <div className='flex gap-8 px-14 items-center justify-center mt-5'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div>
+            <div className='flex gap-8 px-14 items-center justify-center mt-10'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div> 
+            </div>) }
+
+            {isClicked.Retail &&
+             (
+             <div className='flex flex-col flex-wrap'>
+            <div className='flex gap-8 px-14 items-center justify-center mt-5'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+            </div>
+            <div className='flex gap-8 px-14 items-center justify-center mt-10'>
+            <img src="electrician2.jpg" alt="" width="250px"/> 
+           
+            </div> 
+            </div>) }
+
+
+           
         </div>
     </div>
   )
